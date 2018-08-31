5ad5b3bbe3f4be5c31d62e0ca656a195565eff9b
Chris Williams <chris.williams@airbnb.com>
2018-08-31 11:49:15 -0700

    [v0.0.10] more prettier ignore

--- configs/prettier.js
+++ configs/prettier.js
@@ -4,7 +4,7 @@ module.exports = function prettier() {
   return {
     arrowParens: 'avoid',
     bracketSpacing: true,
-    ignore: ['lerna.json', 'package.json', 'node_modules/', 'build/'],
+    ignore: ['lerna.json', 'package.json', 'package-lock.json', 'node_modules/', 'build/', 'lib/', 'esm/'],
     jsxBracketSameLine: false,
     printWidth: 100,
     proseWrap: 'always',
